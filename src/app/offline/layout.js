af11ccf78225ad5fee59efb783be6ea7e96b1aa7
unknown <mubarakmmm5@gmail.com>
2025-08-30 12:23:54 +0400

    debug offline

---
+++ src/app/offline/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: 'Offline - Nexus Digital',
+    description: 'You are currently offline',
+};
+
+export default function OfflineLayout({ children }) {
+    return children;
+}
